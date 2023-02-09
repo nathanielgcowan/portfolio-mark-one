30cf37182a126e5ecb2089582fbfaf6949d82244
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2023-02-09 16:03:34 -0600

    message

--- pages/index.tsx
+++ pages/index.tsx
@@ -127,13 +127,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg object-cover"
-                alt="object-cover"
-                height="100%"
-                layout="responsive"
-                src={web9}
-              />
+
             </div>
           </div>
         </section>
