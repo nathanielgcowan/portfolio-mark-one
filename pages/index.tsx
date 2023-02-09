fa65a996b4319fe26fc2c077b2b69c00ddfb9463
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2023-02-09 15:30:00 -0600

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,6 +101,8 @@ export default function Home() {
               </p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Photoshop</p>
+
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
               <p className="text-gray-800 py-1">Indesign</p>
